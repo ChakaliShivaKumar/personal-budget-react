27f462bf641623d35fba0d01b10ab7ec2fdf6353
Shiva Kumar Chakali <schakali@charlotte.edu>
2025-02-22 22:57:31 -0500

    Week 6 before canvas changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu/Menu';
+import Hero from './Hero/Hero';
+import Homepage from './HomePage/Homepage';
+import Footer from './Footer/Footer';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router className="App">
+      <Menu />
+      <Hero />
+      <div className="mainContainer">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/Menu/Menu.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Menu() {
+  return (
+    <nav
+    role="navigation"
+    aria-label="Main menu"
+    itemScope
+    itemType ='https://schema.org/SiteNavigationElement'
+    >
+    <ul>
+      <li><Link itemProp="url" to="./" title="Homepage">Home</Link></li>
+      <li>
+        <Link
+          itemProp="url"
+          to="./about"
+          title="About page"
+          aria-label="Head to About Page"
+          >About</Link>
+      </li>
+      <li>
+        <Link
+          itemProp="url"
+          to="./login"
+          title="Login page"
+          aria-label="Head to Login Page"
+          >Login</Link>
+      </li>
+    </ul>
+</nav>
+  );
+}
+
+export default Menu;
